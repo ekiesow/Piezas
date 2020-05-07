6a6823b23175d643afcbd29b9fc59de509c73f17
Eric <ekiesow@mail.csuchico.edu>
2020-05-07 00:28:34 -0700

    Added tests for more cases of dropPiece and pieceAt

--- PiezasTest.cpp
+++ PiezasTest.cpp
@@ -30,7 +30,31 @@ TEST(PiezasTest, drop_and_check_single_piece_first_col) {
 TEST(PiezasTest, reset_board) {
   Piezas piezas;
   piezas.dropPiece(0);
-  piezas.pieceAt(0, 0);
   piezas.reset();
   ASSERT_EQ(Piece::Blank, piezas.pieceAt(0, 0));
+}
+
+TEST(PiezasTest, drop_piece_out_of_bounds) {
+  Piezas piezas;
+  ASSERT_EQ(Piece::Invalid, piezas.dropPiece(4));
+}
+
+TEST(PiezasTest, drop_piece_out_of_bounds_negative) {
+  Piezas piezas;
+  ASSERT_EQ(Piece::Invalid, piezas.dropPiece(-1));
+}
+
+TEST(PiezasTest, drop_and_check_stacked_pieces_first_col) {
+  Piezas piezas;
+  piezas.dropPiece(0);  // X
+  piezas.dropPiece(0);  // O
+  ASSERT_EQ(Piece::O, piezas.pieceAt(0, 0));
+}
+
+TEST(PiezasTest, fill_first_col_check_top_piece) {
+  Piezas piezas;
+  piezas.dropPiece(0);  // X
+  piezas.dropPiece(0);  // O
+  piezas.dropPiece(0);  // X
+  ASSERT_EQ(Piece::X, piezas.pieceAt(2, 0));
 }
